4fd0003180519ed6515d83644c7bdeecee176537
Lorenzo Santinelli <lorenzofsantinelli@gmail.com>
2026-02-19 17:00:08 +0100

    Add profile page and update video upload functionality
    - Created a new profile page to display user information and uploaded videos.
    - Updated the upload video page to include a title input field.
    - Added profile view in views.py to handle profile data retrieval.
    - Updated URLs to include the new profile route and serve media files in development.
    - Configured MEDIA_URL and MEDIA_ROOT in settings.py for video storage.

--- se_test/urls.py
+++ se_test/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import login_view, signup_view, home_view, landing_view, upload_view
+from main.views import home_view, login_view, signup_view, landing_view, upload_view, profile_view
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.conf.urls.static import static
@@ -12,4 +12,9 @@ urlpatterns = [
     path("login/", login_view, name="login"),
     path("signup/", signup_view, name="signup"),
     path("upload_video/", upload_view, name="upload"),
+    path("profile/", profile_view, name="profile")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, 
+                          document_root=settings.MEDIA_ROOT)
