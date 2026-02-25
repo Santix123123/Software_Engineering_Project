8264176418217af55e9611aa29e9b9287cc4648c
Lorenzo Santinelli <lorenzofsantinelli@gmail.com>
2026-02-25 17:53:00 +0100

    Add video display functionality with AJAX and time ago feature
    - Implemented a new JavaScript file `show_video.js` to handle video loading.
    - Added a function to display the time since the video was uploaded.
    - Utilized AJAX to fetch video details and dynamically update the video container.
    - Included buttons for like, share, and comment actions.
    - Handled error case for video not found.

--- se_test/urls.py
+++ se_test/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import home_view, login_view, signup_view, landing_view, upload_view, profile_view, show_video_view
+from main.views import home_view, login_view, signup_view, landing_view, upload_view, profile_view, show_video_view, video_detail
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.conf.urls.static import static
@@ -14,6 +14,7 @@ urlpatterns = [
     path("upload_video/", upload_view, name="upload"),
     path("profile/", profile_view, name="profile"),
     path("video/<int:video_id>/", show_video_view, name="show_video"),
+    path("videos/<int:video_id>/", video_detail, name="video-detail"),
 ]
 
 if settings.DEBUG:
