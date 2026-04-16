b0226040fe4c76cf729aa9caff2d609a94d4a071
Lorenzo Santinelli <lorenzofsantinelli@gmail.com>
2026-04-16 19:03:48 +0200

    Added Kubernetes

--- se_test/urls.py
+++ se_test/urls.py
@@ -1,21 +1,37 @@
+"""
+URL configuration for se_test project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
-from main.views import home_view, login_view, signup_view, landing_view, upload_view, profile_view, show_video_view, video_detail, videos_view, upload_video_api
-from django.contrib.auth.decorators import login_required
+# from main.views import home_view, login_view, signup_view, upload_view, logout_view, profile_view, show_video_view, video_detail
 from main.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path("", home_view, name="home"),
-    path("landing/", landing_view, name="landing"),
     path("login/", login_view, name="login"),
     path("signup/", signup_view, name="signup"),
-    path("upload_video/", upload_view, name="upload"),
+    path("upload/", upload_view, name="upload"),
     path("profile/", profile_view, name="profile"),
+    path("logout/", logout_view, name="logout"),
     path("video/<int:video_id>/", show_video_view, name="show_video"),
-
+    
     path("videos/<int:video_id>/", video_detail, name="video-detail"),
     path("videos/", videos_view, name="videos"),
     path("videos/upload/", upload_video_api, name="video-upload-api"),
@@ -23,3 +39,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+elif settings.SERVE_MEDIA:
+    urlpatterns += [
+        path("media/<path:path>", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
